893e98d5db5c65170f6b171b3f417b5795111a0c
Kelsey.Chen <naoyakaoru@gmail.com>
2020-08-25 16:20:34 +0800

    task4 fin

--- job.js
+++ job.js
@@ -1,13 +1,12 @@
 document.addEventListener('DOMContentLoaded', async function(){
   const jobContainer = document.getElementById('job-pannel')
-  const getJobs = await fetch('https://still-spire-37210.herokuapp.com/positions.json')
-  const jobs = await getJobs.json()
   const form = document.forms['search-job']
   const formSearch = form.querySelector('[type=submit]')
   var a = 1
-  
+  giveMeJobs('')
 
   const more = document.querySelector('.pagination-next')
+  
   formSearch.addEventListener('click', function(e){
     var a = 1
     e.preventDefault()
@@ -20,12 +19,17 @@ document.addEventListener('DOMContentLoaded', async function(){
     giveMeJobs(a)
   })
   function queryArea(p){
-    const description = form.description.value
-    const location = form.location.value
-    const isFullTime = form.full_time.checked
+    if(p==''){
+      const description = form.description.value
+      const location = form.location.value
+      const isFullTime = form.full_time.checked
 
-    let query =`${isFullTime ? 'full_time=on' : ''}${description ? '&description=' + description : ''}${location ? '&location=' + location : ''}${p ? '&page=' + p : ''}`
-    return 'https://still-spire-37210.herokuapp.com/positions.json?' + query
+      let query =`${isFullTime ? 'full_time=on' : ''}${description ? '&description=' + description : ''}${location ? '&location=' + location : ''}${p ? '&page=' + p : ''}`
+      return 'https://still-spire-37210.herokuapp.com/positions.json?' + query
+    }else{
+      let query = `${p ? '?page=' + p : ''}`
+      return 'https://still-spire-37210.herokuapp.com/positions.json' + query
+    }
   }
     function giveMeJobs(x){ fetch(queryArea(x))
       .then(res => res.json())
