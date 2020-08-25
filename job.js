f178b1f4ecec35451306f44a87dad502dff24d73
Kelsey.Chen <naoyakaoru@gmail.com>
2020-08-25 16:03:17 +0800

    paginate done

--- job.js
+++ job.js
@@ -1,40 +1,35 @@
 document.addEventListener('DOMContentLoaded', async function(){
   const jobContainer = document.getElementById('job-pannel')
-
   const getJobs = await fetch('https://still-spire-37210.herokuapp.com/positions.json')
   const jobs = await getJobs.json()
-
   const form = document.forms['search-job']
   const formSearch = form.querySelector('[type=submit]')
+  var a = 1
+  
 
+  const more = document.querySelector('.pagination-next')
   formSearch.addEventListener('click', function(e){
+    var a = 1
     e.preventDefault()
-    let a = 1
-    const more = document.querySelector('.pagination-next')
-    function queryArea(p){
-      const description = form.description.value
-      const location = form.location.value
-      const isFullTime = form.full_time.checked
-
-      let query =`${isFullTime ? 'full_time=on' : ''}${description ? '&description=' + description : ''}${location ? '&location=' + location : ''}${p ? '&page=' + p : ''}`
-      return 'https://still-spire-37210.herokuapp.com/positions.json?' + query
+    if (a==1) {
+      jobContainer.innerHTML = ''
+      giveMeJobs(0)
     }
-    jobContainer.innerHTML = ''
-    fetch(queryArea())
+  })
+  more.addEventListener('click', function(){
+    giveMeJobs(a)
+  })
+  function queryArea(p){
+    const description = form.description.value
+    const location = form.location.value
+    const isFullTime = form.full_time.checked
+
+    let query =`${isFullTime ? 'full_time=on' : ''}${description ? '&description=' + description : ''}${location ? '&location=' + location : ''}${p ? '&page=' + p : ''}`
+    return 'https://still-spire-37210.herokuapp.com/positions.json?' + query
+  }
+    function giveMeJobs(x){ fetch(queryArea(x))
       .then(res => res.json())
       .then(jobRes => {
-        if (jobRes.length = 50){
-          a = a + 1
-          more.removeAttribute('disabled')
-          more.addEventListener('click', function(){
-            console.log(queryArea(a))
-            fetch(queryArea(a))
-              .then(res => res.json())
-              .then(jobRes => {
-                console.log(jobRes)
-              })
-          })
-        }
         jobRes.forEach(job => {
           const jobTemplate = document.querySelector('#job-template')
           let newJob = document.createElement('tr')
@@ -48,12 +43,14 @@ document.addEventListener('DOMContentLoaded', async function(){
           newJob.querySelector('.company').setAttribute('href', job.company_url)
           jobContainer.insertAdjacentElement('beforeend', newJob)
         })
-        
-    })
+        console.log(jobRes.length)
+        console.log(queryArea(x))
+        if (jobRes.length == 50){
+          a = a + 1
+          more.removeAttribute('disabled')
+        }else{
+          more.setAttribute('disabled','')
+        }
+    })}
     
-
-  
-    
-
-  })
 })
