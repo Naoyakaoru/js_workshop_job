87df051b136e0e56c627361789efd2f41b4092a7
Kelsey.Chen <naoyakaoru@gmail.com>
2020-08-25 15:16:49 +0800

    fin

--- job.js
+++ job.js
@@ -1,14 +1,12 @@
 document.addEventListener('DOMContentLoaded', async function(){
-  const jobTemplate = document.getElementById('job-template')
   const jobContainer = document.getElementById('job-pannel')
 
   const getJobs = await fetch('https://still-spire-37210.herokuapp.com/positions.json')
   const jobs = await getJobs.json()
-  console.log(jobs)
 
   const form = document.forms['search-job']
-
   const formSearch = form.querySelector('[type=submit]')
+
   formSearch.addEventListener('click', function(e){
     e.preventDefault()
     function queryArea(){
@@ -18,15 +16,32 @@ document.addEventListener('DOMContentLoaded', async function(){
       let currentLocation = window.location.href
 
       let query =`${isFullTime ? 'full_time=on' : ''}${description ? '&description=' + description : ''}${location ? '&location=' + location : ''}`
-      currentLocation = '/' + query
       return 'https://still-spire-37210.herokuapp.com/positions.json?' + query
     }
     console.log(queryArea())
+    jobContainer.innerHTML = ''
     fetch(queryArea())
       .then(res => res.json())
       .then(jobRes => {
-        console.log(jobRes)
-      } )
+        jobRes.forEach(job => {
+          const jobTemplate = document.querySelector('#job-template')
+          let newJob = document.createElement('tr')
+          newJob.innerHTML = jobTemplate.innerHTML
+          console.log(newJob)
+          newJob.querySelector('.fulltime').textContent = job.type
+          newJob.querySelector('.job-url').setAttribute('href', job.url)
+          newJob.querySelector('.job-url').textContent = job.title
+          newJob.querySelector('.location').textContent = job.location
+          newJob.querySelector('.company').textContent = job.company
+          newJob.querySelector('.company').setAttribute('href', job.company_url)
+          jobContainer.insertAdjacentElement('beforeend', newJob)
+        })
+        
+    })
     
+
+  
+    
+
   })
 })
